d89c3e0171d8677a07193fdbeb5032e49724be49
Sailuja Sathiyalogan <134766379+sailuja-sathiyalogan@users.noreply.github.com>
2023-06-02 09:18:13 +0530

    Add files via upload

---
+++ activity 1.py
@@ -0,0 +1,17 @@
+Python 3.10.11 (tags/v3.10.11:7d4cc5a, Apr  5 2023, 00:38:17) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+= RESTART: C:/Users/HP/AppData/Local/Programs/Python/Python310/myfistprogram.py
+Hello, World!
+What is your name? Sailuja
+Hello  Sailuja
+>>> 
+>>> age=11
+>>> print(age)
+11
+>>> weight=37
+>>> print(weight)
+37
+>>> height=152
+>>> print(height)
+152
